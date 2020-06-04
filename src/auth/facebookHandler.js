a3165b3f9dfbc7360ea742cb09adcd34d256e79d
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-06-04 12:15:46 +0700

    test facebook login

--- src/auth/facebookHandler.js
+++ src/auth/facebookHandler.js
@@ -5,7 +5,7 @@ exports.loginFacebook = passport.authenticate('facebook', {scope: 'email'})
 exports.facebookAuth = function(req, res, next) {
     passport.authenticate("facebook", function(err, user){
         if(err) return res.redirect(`${process.env.CLIENT}/login`)
-            console.log(user)
+            console.log('facebookAUth',user)
         return res.redirect(`${process.env.CLIENT}/?token=${user.tokens[user.tokens.length-1]}`)
     })(req, res, next)
 }
